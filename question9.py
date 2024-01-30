4677f5446594b6fae97d22225d9ced4d9d770d54
Sukrith-01 <117061271+Sukrith-01@users.noreply.github.com>
2024-01-29 23:33:22 -0500

    Add files via upload

--- question9.py
+++ question9.py
@@ -1,18 +1,19 @@
 def question9_1():
-    """
-    answer = string'
-    """
+    answer = 'SMC'
     return answer
 
 def question9_2():
+    answer = 'Jaccard'
     return answer
 
 def question9_3():
+    answer = 'Euclidean'
     return answer 
 
 def question9_4():
+    answer = 'Euclidean'
     return answer 
 
 def question9_5():
-    return answer 
-
+    answer = 'Cosine Similarity'
+    return answer
